161d0a60b8bdefaf8ae763fce17f21969a351484
hicadok105@ridteam.com <hicadok105@ridteam.com>
2024-03-10 12:54:08 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter number : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int p = 1;
+for (int i = 1; i <= num; i++)
+{
+    p *= i;
+}
+Console.WriteLine($"{num} faktorial = "+p);
